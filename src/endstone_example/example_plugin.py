2edcd63dfe7e0d466a05394c3e67963689066d5d
Vincent <magicdroidx@gmail.com>
2024-03-07 16:17:30 +0000

    feat: use pattern matching to handle different command cases

--- src/endstone_example/example_plugin.py
+++ src/endstone_example/example_plugin.py
@@ -1,5 +1,6 @@
 from endstone.command import Command, CommandSender
 from endstone.plugin import Plugin
+
 from endstone_example.python_command import PythonCommand, PythonCommandExecutor
 from endstone_example.test_command import TestCommand
 
@@ -19,9 +20,18 @@ class ExamplePlugin(Plugin):
 
     def on_command(self, sender: CommandSender, command: Command, args: list[str]) -> bool:
         # You can also handle commands here instead of setting an executor in on_enable if you prefer
-        if not args:
-            sender.send_message(f"/{command.name} is executed from Python!")
-        else:
-            sender.send_message(f"/{command.name} is executed from Python with argument {args[0]}!")
+        match (command.name, args):
+            case ("test", []):
+                # handle /test
+                sender.send_message("Test!")
+            case ("test", [n]):
+                # handle /test n
+                sender.send_message(f"Test with number {n}!")
+            case (_, []):
+                # handle /* (wildcard)
+                sender.send_message(f"/{command.name} is executed from Python!")
+            case (_, *args):
+                # handle /* args... (wildcard)
+                sender.send_message(f"/{command.name} is executed from Python with arguments {args}!")
 
         return True
